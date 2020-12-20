49aacb16975f27af68fcf57a6c6b3b9384ccab43
macbookpro <emrefikirlier@gmail.com>
2020-12-20 16:05:14 +0300

    master config

--- php/Core.php
+++ php/Core.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 
-    define("WEBURL","http://localhost:8888/rehberokul-web/");
+    define("WEBURL","https://www.rehberokul.com/");
     define("SITENAME","Rehber Okul");
     define("INFOSECVAL","e057857db2cef2bdb5d89fa91505d499bd87d975");
     define("SUBFOLDER", false);
@@ -9,7 +9,7 @@
     require_once('Classes/MysqliDb.php');
     require_once('Classes/Security.php');
 
-    $db = new MysqliDb('localhost:8889', 'root', 'root', 'rehberok_test_db');
+    $db = new MysqliDb('localhost', 'rehberok_web_user', 'P(ie*igC*juoRehberO', 'rehberok_web_db');
     $sec = new Security;
 
     $_SESSION['csrf_token'] = base64_encode(openssl_random_pseudo_bytes(32));
